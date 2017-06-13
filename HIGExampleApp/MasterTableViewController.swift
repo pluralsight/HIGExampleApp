7d2dc83022657bb47d30521b6b56e0cc0e1eadc3
Matt <mattmcmurry@gmail.com>
2017-06-13 09:49:07 -0600

    Setup initial layout and top level menu.

---
+++ HIGExampleApp/MasterTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.swift
+//  HIGExampleApp
+//
+//  Created by Dev Team on 6/12/17.
+//  Copyright © 2017 Pluralsight. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewController: UITableViewController {
+
+}
