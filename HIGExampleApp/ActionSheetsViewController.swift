d835d6ca437541be31da8aeed693c08382122938
Matt <mattmcmurry@gmail.com>
2017-06-19 11:37:48 -0600

    Alerts and Action Sheets

---
+++ HIGExampleApp/ActionSheetsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ActionSheetsViewController.swift
+//  HIGExampleApp
+//
+//  Created by Dev Team on 6/19/17.
+//  Copyright © 2017 Pluralsight. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetsViewController: UIViewController {
+
+    @IBAction func toggleActionSheet(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "Action Sheet Title", message: "This is an action sheet message", preferredStyle: .actionSheet)
+        
+        let defaultAction = UIAlertAction(title: "Default Action", style: .default, handler: nil)
+        let destructiveAction = UIAlertAction(title: "Destructive Action", style: .destructive, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionSheet.addAction(defaultAction)
+        actionSheet.addAction(destructiveAction)
+        actionSheet.addAction(cancelAction)
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+}
