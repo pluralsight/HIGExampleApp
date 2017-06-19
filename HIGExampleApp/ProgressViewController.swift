973ae9100a7980266f6a05bade8d702dc391df62
Matt <mattmcmurry@gmail.com>
2017-06-19 10:21:39 -0600

    Added progress indicators.

---
+++ HIGExampleApp/ProgressViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProgressViewController.swift
+//  HIGExampleApp
+//
+//  Created by Dev Team on 6/19/17.
+//  Copyright © 2017 Pluralsight. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    @IBOutlet weak var topSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var middleSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var bottomSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func topSpinnerToggle(_ sender: Any) {
+        topSpinner.spinning = !topSpinner.spinning
+    }
+    
+    @IBAction func middleSpinnerToggle(_ sender: Any) {
+        middleSpinner.spinning = !middleSpinner.spinning
+    }
+    
+    @IBAction func bottomSpinnerToggle(_ sender: Any) {
+        bottomSpinner.spinning = !bottomSpinner.spinning
+    }
+    
+    @IBAction func networkSpinnerToggle(_ sender: Any) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = !UIApplication.shared.isNetworkActivityIndicatorVisible
+    }
+    
+    @IBAction func toggleProgressBar(_ sender: Any) {
+        progressBar.setProgress(0, animated: false)
+        DispatchQueue.main.async {
+            self.progressBar.setProgress(1, animated: true)
+        }
+    }
+}
+
+extension UIActivityIndicatorView {
+    
+    var spinning: Bool {
+        get {
+            return isAnimating
+        }
+        set {
+            if newValue {
+                startAnimating()
+            } else {
+                stopAnimating()
+            }
+        }
+    }
+    
+}
